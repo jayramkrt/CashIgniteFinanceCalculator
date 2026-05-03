9504cdd6e981135b8cfaf1980b1ca314a956495d
jayramkrt <jayramkrt@gmail.com>
2026-05-03 21:47:34 +0530

    added vercel analytics code changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
